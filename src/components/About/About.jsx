1ab97274c34e115020fec7d76acc26e4402a9a39
Roshani Dattu Borase <shriradh07@gmail.com>
2025-04-12 11:23:05 +0530

    Add files via upload

---
+++ src/components/About/About.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>
+                I'm Roshani Borase, a 2nd-year Computer Engineering student with a passion for frontend development. I build responsive, interactive, and modern web interfaces using React.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Learning Backend</h3>
+              <p>
+                I'm currently exploring backend development to build full-stack applications. I'm gaining experience with APIs and server-side logic.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/uiIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>UI Designer</h3>
+              <p>
+                I enjoy crafting clean and user-friendly designs. I’ve worked on landing pages and love turning ideas into smooth user experiences.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
