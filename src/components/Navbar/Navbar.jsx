6fb34876d9f0b867a764701b81e10295ebd5b9f9
Roshani Dattu Borase <shriradh07@gmail.com>
2025-04-12 19:00:14 +0530

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -29,9 +29,8 @@ export const Navbar = () => {
           <li>
             <a href="#about">About</a>
           </li>
-          <li>
-            <a href="#experience">Experience</a>
-          </li>
+          
+          
            
           <li>
             <a href="#contact">Contact</a>
